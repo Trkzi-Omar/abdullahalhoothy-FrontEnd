b27c18a1e873a2882d07499021df7870aad9be2e
“Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2025-06-10 13:45:46 +0100

    refactor: remove unused files

--- src/components/CatalogDetailsForm/CatalogDetailsForm.tsx
+++ src/components/CatalogDetailsForm/CatalogDetailsForm.tsx
@@ -89,7 +89,7 @@ function CatalogDetailsForm() {
             Subscription Price
           </label>
           <input
-            type="text"
+            type="number"
             id="subprice"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             value={subscriptionPrice || '0'}

6f5665283ff5f7f8d6719bcce5f405c1a44afbd2
abdullah abbas <abdullahalhoothy92@gmail.com>
2025-10-29 17:15:38 -0400

    refactor: simplify StaticRedirect component by removing backend URL logic and using static file URL directly

--- src/components/StaticRedirect/StaticRedirect.tsx
+++ src/components/StaticRedirect/StaticRedirect.tsx
@@ -1,40 +1,15 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const StaticRedirect = () => {
   const params = useParams();
-  const [backendUrl, setBackendUrl] = useState<string>('');
 
   const filePath = params['*'];
-
-  useEffect(() => {
-    // Determine the backend URL based on the current hostname
-    const currentHost = window.location.hostname;
-    let backendHost: string;
-
-    if (currentHost === 'localhost') {
-      backendHost = 'http://localhost:8000';
-    } else if (currentHost === '37.27.195.216') {
-      backendHost = 'http://37.27.195.216:8000';
-    } else if (currentHost === 's-locator.northernacs.com') {
-      backendHost = 'http://s-locator.northernacs.com:8000';
-    } else {
-      // Fallback to localhost
-      backendHost = 'http://localhost:8000';
-    }
-
-    const fullUrl = `${backendHost}/static/${filePath}`;
-    setBackendUrl(fullUrl);
-  }, [filePath]);
-
-  if (!backendUrl) {
-    return <div>Loading...</div>;
-  }
+  const staticFileUrl = `/static/${filePath}`;
 
   return (
     <div className="w-full h-full">
       <iframe
-        src={backendUrl}
+        src={staticFileUrl}
         className="w-full h-full border-none"
         title="Static Content"
       />
